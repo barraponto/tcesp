cb365415b593b21dac03ddcd7b6af2977589a891
Capi Etheriel <barraponto@gmail.com>
2011-10-03 16:50:55 -0300

    theming the header region

--- template.php
+++ template.php
@@ -3,3 +3,9 @@ function tcesp_preprocess_page($vars) {
   drupal_set_html_head('<link href="http://fonts.googleapis.com/css?family=Amaranth:400" rel="stylesheet" type="text/css">');
   $vars['head'] .= drupal_get_html_head();
 }
+
+function tcesp_preprocess_block($vars) {
+  if ($vars['block']->region == 'header') {
+    $vars['block']->subject = '<span>' . $vars['block']->subject . '</span>';
+  }
+}
